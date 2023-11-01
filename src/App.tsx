fa454cfca21ca9209a6b36e345c1b2c17bafc2b4
Phillip <nzau78@gmail.com>
2023-11-01 05:47:28 +0300

    Project initialisation: Setting up project with vite and adding tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-2xl text-red-500">Hey</p>
+    </>
+  );
+}
+
+export default App;
